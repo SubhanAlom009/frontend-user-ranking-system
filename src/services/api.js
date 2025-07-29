982d03f80f4a9680d4cbb2e2a3b2a4554a9b9024
SubhanAlom009 <subhanalombasic123@gmail.com>
2025-07-29 20:02:21 +0530

    updated urls

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,16 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://backend-user-ranking-system.onrender.com", // change if deployed
+  baseURL: "https://backend-user-ranking-system.onrender.com/api", // baseURL now includes /api
 });
 
 // ========== USERS ==========
 
 export const getUsers = () => API.get("/users");
-export const getLeaderboard = () => API.get("/claims/leaderboard");
-
-export const claimPoints = (userId) => API.post(`/claims/${userId}`);
-
 export const addUser = (userData) => API.post("/users", userData);
-
-// (Optional) get specific user by id
 export const getUserById = (userId) => API.get(`/users/${userId}`);
+
+// ========== CLAIMS ==========
+
+export const getLeaderboard = () => API.get("/claims/leaderboard");
+export const claimPoints = (userId) => API.post(`/claims/${userId}`);
