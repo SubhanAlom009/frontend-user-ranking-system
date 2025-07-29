1c5d82c4681a160f726052a880ef2e645f70b938
SubhanAlom009 <subhanalombasic123@gmail.com>
2025-07-29 19:49:55 +0530

    updated baseurl

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api", // change if deployed
+  baseURL: "https://backend-user-ranking-system.onrender.com", // change if deployed
 });
 
 // ========== USERS ==========
